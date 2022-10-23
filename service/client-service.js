c5a194452a39f18a6f24a9181e6048b6a8d02037
Frank-Grijalba <fjgrijalba@gmail.com>
2022-10-23 16:35:54 -0500

    Creando template

--- service/client-service.js
+++ service/client-service.js
@@ -1,3 +1,34 @@
+const crearNuevaLinea = (nombre, email) => {
+    const linea = document.createElement("tr")
+    const contenido = `
+        <td class="td" data-td>${nombre}</td>
+        <td>${email}</td>
+        <td>
+        <ul class="table__button-control">
+            <li>
+            <a
+                href="../screens/editar_cliente.html"
+                class="simple-button simple-button--edit"
+                >Editar</a
+            >
+            </li>
+            <li>
+            <button
+                class="simple-button simple-button--delete"
+                type="button"
+            >
+                Eliminar
+            </button>
+            </li>
+        </ul>
+        </td>
+    `;
+    linea.innerHTML = contenido;
+    return linea;
+}
+
+const tabla = document.querySelector("[data-table]");
+
 const http = new XMLHttpRequest();
 
 //name_from_variable.open  receive 2 parameters (method, url)
@@ -12,7 +43,11 @@ http.open('GET','http://localhost:3000/perfil');
 http.send();
 
 http.onload = () => {
-    const data = http.response;
-    console.log(data);
+    const data =  JSON.parse(http.response);
+    data.forEach( perfil => {
+        const nuevaLinea = crearNuevaLinea(perfil.nombre, perfil.email);
+        tabla.appendChild(nuevaLinea);
+    });
+
 }
 
