8201246807d15bb710e5737f77951f961d3cf8e2
Frank-Grijalba <fjgrijalba@gmail.com>
2022-10-27 11:32:57 -0500

    registro-completado

--- controllers/registro.controller.js
+++ controllers/registro.controller.js
@@ -8,7 +8,7 @@ formulario.addEventListener("submit", (evento) => {
     const email = document.querySelector("[data-email]").value;
     console.log(nombre,"-", email);
     clientServices.crearCliente(nombre, email).then( respuesta => {
-        console.log(respuesta)
+        window.location.href = "/screens/registro_completado.html";
     } ).catch( (error) => console.log(error) )
 });
 
