158e7a438488d67869ad7b7c63b493cdf4d54c78
Frank-Grijalba <fjgrijalba@gmail.com>
2022-10-31 16:37:16 -0500

    modificar-id

--- service/client-service.js
+++ service/client-service.js
@@ -18,8 +18,14 @@ const eliminarCliente = (id) => {
     });
 };
 
+const detalleCliente = (id) => {
+    return fetch(`http://localhost:3000/perfil/${id}`).then((respuesta) => respuesta.json()
+    );
+};
+
 export const clientServices = { 
     listaCLientes,
     crearCliente,
     eliminarCliente,
+    detalleCliente,
 };
